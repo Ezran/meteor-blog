8d258c3e6813f7d4935390fa5c273a92c37fe663
Ezran <phantom.ezran@gmail.com>
2015-09-30 18:08:04 -0600

    Basic blog functionality and input box css

--- simple-blog.js
+++ simple-blog.js
@@ -1,23 +1,25 @@
+Posts = new Mongo.Collection("posts");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    posts: function() {
+      return Posts.find({}, {sort: {createdAt: -1}});
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  
+  Template.body.events({
+    "submit .new-post": function (event) {
+      event.preventDefault();
+      
+      Posts.insert({
+        title: event.target.title.value,
+        date: new Date(),
+        body: event.target.body.value
+      });
+      
+      event.target.title.value = "";
+      event.target.body.value = "";
     }
   });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
 }
