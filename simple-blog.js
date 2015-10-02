cea2b87ce1ec5e99acfc92ce8fe5804b16a9eb6b
Ezran <phantom.ezran@gmail.com>
2015-10-02 12:26:12 -0600

    Basic CSS implemented, delete button added

--- simple-blog.js
+++ simple-blog.js
@@ -1,25 +1,48 @@
 Posts = new Mongo.Collection("posts");
 
 if (Meteor.isClient) {
+  
+  Template.registerHelper("simpleDate", function (ts) {
+    return moment(ts).fromNow();
+  });
+  
   Template.body.helpers({
     posts: function() {
-      return Posts.find({}, {sort: {createdAt: -1}});
+      return Posts.find({}, {sort: {date: -1}});
+    },
+    checked: function() {
+      return Session.get("checked");
     }
   });
   
   Template.body.events({
-    "submit .new-post": function (event) {
+    "submit .newPost": function (event) {
       event.preventDefault();
       
-      Posts.insert({
-        title: event.target.title.value,
-        date: new Date(),
-        body: event.target.body.value
-      });
+      var t = event.target.title.value;
+      var b = event.target.body.value;
       
-      event.target.title.value = "";
-      event.target.body.value = "";
+      if (t == "" || b == "")
+      {
+        //throw invalid input errors 
+      }
+      else {
+        Posts.insert({
+          title: t,
+          date: new Date(),
+          body: b
+        });
+        
+        event.target.title.value = "";
+        event.target.body.value = "";
+      }
+    },
+    "click .dropdownToggleLabel": function() {
+      Session.set("checked",!Session.get("checked"));
+    },
+    "click .deletePost" : function () {
+      //var target = Posts.findOne(this._id);
+      Posts.remove(this._id);
     }
-  });
-  
+  });  
 }
